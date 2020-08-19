ac1aee0d7887a2878e6cffa34384b0828a552352
Will Crawford <willcrawford@Parkers-MacBook-Air.local>
2020-08-19 09:53:50 -0400

    added controllers and models. set up all inital files

---
+++ models/skill.js
@@ -0,0 +1,7 @@
+const skills = [
+    {id: 125223, skill: 'JavaScript', mastered: false},
+    {id: 127904, skill: 'HTML', mastered: true},
+    {id: 139608, skill: 'CSS', mastered: false}
+    {id: 123008, skill: 'MatLab', mastered: false}
+
+];
