7cb5833a667d12bc6383dd906104f8480646fa63
Will Crawford <willcrawford@Parkers-MacBook-Air.local>
2020-08-19 10:31:39 -0400

    part one of the lab complete

--- models/skill.js
+++ models/skill.js
@@ -1,7 +1,19 @@
 const skills = [
     {id: 125223, skill: 'JavaScript', mastered: false},
     {id: 127904, skill: 'HTML', mastered: true},
-    {id: 139608, skill: 'CSS', mastered: false}
+    {id: 139608, skill: 'CSS', mastered: false},
     {id: 123008, skill: 'MatLab', mastered: false}
-
 ];
+
+module.exports = {
+    getAll,
+    getOne
+}
+
+function getAll() {
+    return skills
+}
+
+function getOne(id) {
+    return skills.find(skill => skill.id === parseInt(id))
+}
--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const skillsCtrl = require('../controllers/skills')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.get('/', skillsCtrl.index)
+router.get('/:id', skillsCtrl.show)
 module.exports = router;
